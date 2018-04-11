02fd6355b3ac41c8d749de18b69889c219839505
LAPTOPPC\U <melnik@g.jct.ac.il>
2018-04-11 22:02:56 +0300

    הוספת קבצים

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import { IProduct } from './product';
+
+@Injectable()
+export class ProductService {
+    private _productUrl = './api/products/products.json';
+    getProducts():Observable<IProduct[]>{
+        return this._http.get<IProduct[]>(this._productUrl)
+        .do(data => console.log('All: ' + JSON.stringify(data)))
+        ;
+          }
+
+          getProductsByID(id:number):void
+          {
+        
+          }
+    constructor(private _http: HttpClient){    }
+}
---
+++ src/app/products/product.ts
@@ -0,0 +1,16 @@
+export interface IProduct{
+    productId :number; 
+    productName :string;
+    productCode :string;
+    releaseDate :string;
+    description :string;
+    price :number;
+    starRating : number;
+    imageUrl : string;
+
+
+
+
+
+
+}
